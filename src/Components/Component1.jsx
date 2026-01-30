479092ddd3ff54e9352c393dd7a0836a2474c5af
PROGMVBPR <progmvbpr.progmaster@gmail.com>
2026-01-30 12:34:43 +0530

    Initial commit

---
+++ src/Components/Component1.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Component1()
+{
+    return(
+    
+    <div>
+        <h2>Component1</h2>
+    </div>
+        
+    )
+}
+
+export default Component1;
---
+++ src/Components/Component2.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+function Component2(){
+    return(
+        <h1>Component2</h1>
+    )
+    
+}
+
+export default Component2;
